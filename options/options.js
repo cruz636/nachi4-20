55850dd34c08352c3d86cba291506657c6fb4101
Juan Barneix <juanbarcruz96@gmail.com>
2023-03-31 16:39:58 -0300

    update

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nachi4-20",
+  url: "https://nachi4-20.botics.co",
+  api: "https://nachi4-20.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
